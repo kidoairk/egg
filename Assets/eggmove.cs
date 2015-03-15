2a163795f0a77866df4f3ba16f5afba73fdd0068
t-koyama <mayatake29@gmail.com>
2015-03-15 18:15:20 +0900

    initail commit

---
+++ Assets/eggmove.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eggmove : MonoBehaviour {
+	private bool PRE = false;
+	private bool PC = false;
+	private bool NEW = true;
+	
+	private bool startFlag = false;
+	
+	// Label
+	private GUIStyle labelStyle;
+	
+	private float xac = 0.0f;
+	private float yac = 0.0f;
+	private float zac = 0.0f;
+	
+	// initial
+	void Awake(){
+		// frame rate
+		Application.targetFrameRate = 30;
+	}
+	
+	// Use this for initialization
+	void Start () {
+		this.labelStyle = new GUIStyle();
+		this.labelStyle.fontSize = Screen.height/30;
+		this.labelStyle.normal.textColor = Color.white;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		xac = 0.1f * xac + 0.1f * Input.acceleration.x;
+		yac = 0.1f * yac + 0.1f * Input.acceleration.y;
+		zac = 0.1f * zac + 0.1f * Input.acceleration.z;
+		transform.position += new Vector3 (xac, zac, yac);
+	}
+	
+	void OnGUI(){
+		float x = Screen.width / 10;
+		float y = 0;
+		float w = Screen.width * 8 / 10;
+		float h = Screen.height / 20;
+		
+		float xaccel = 0;
+		float yaccel = 0;
+		float zaccel = 0;
+		
+		for (int i = 0; i < 3; i++){
+			y = Screen.height / 10 + h * i;
+			string text = string.Empty;
+			
+			switch (i){
+			case 0://X
+				xaccel = 0.9f * xaccel + 0.1f * Input.acceleration.x;
+				text = string.Format("X:{0}", System.Math.Round(xaccel, 2));
+				break;
+			case 1://Y
+				yaccel = 0.9f * yaccel + 0.1f * Input.acceleration.y;
+				text = string.Format("Y:{0}", System.Math.Round(yaccel, 2));
+				break;
+			case 2://Z
+				zaccel = 0.9f * zaccel + 0.1f * Input.acceleration.z;
+				text = string.Format("Z:{0}", System.Math.Round(zaccel, 2));
+				break;
+			default:
+				throw new System.InvalidOperationException();
+			}
+			GUI.Label(new Rect(x, y, w, h), text, this.labelStyle);
+		}
+	}
+}
